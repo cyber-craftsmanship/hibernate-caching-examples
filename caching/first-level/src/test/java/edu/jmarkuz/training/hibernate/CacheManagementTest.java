0476a9c1de71e2a81c29b2deaaf7c39d5f1b26da
marian.kuziv <mi.kuziv@gmail.com>
2024-02-01 22:05:00 +0200

    Test cache detaching action

---
+++ caching/first-level/src/test/java/edu/jmarkuz/training/hibernate/CacheManagementTest.java
@@ -0,0 +1,19 @@
+package edu.jmarkuz.training.hibernate;
+
+import edu.jmarkuz.training.hibernate.model.Author;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CacheManagementTest extends BaseTest {
+
+    @Test
+    public void testCacheDetachingAction() {
+        doInTransaction(entityManager -> {
+            var author_1 = entityManager.find(Author.class, 1L);
+            Assert.assertTrue(entityManager.contains(author_1));
+            // detaching author_1
+            entityManager.detach(author_1);
+            Assert.assertFalse(entityManager.contains(author_1));
+        });
+    }
+}
